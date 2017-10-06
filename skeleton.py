b47dad9808ccef7f53319463abd23593a4e9f06b
sudo-Whateverman <Nick.Kuhlik@gmail.com>
2017-10-07 02:35:59 +0300

    -- When clicked, a single frame will be filled with rectangular green fill.
    -- TODO: add permanent marks on video.

---
+++ skeleton.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+
+
+def pick_category():
+    pass
+
+
+def classify():
+    pass
+
+
+def save_model():
+    pass
+
+
+def test_model():
+    pass
+
--- videoSource.py
+++ videoSource.py
@@ -7,18 +7,42 @@ def sourceCapture():
     ret, frame = cap.read()
     if cap.isOpened() :
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
-        return cap
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(gray, 'From Webcam', (10, 50), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        return gray
     else:
         print "Nothing to show up for"
         return 0
 
+def point2pick():
+    cv2.setMouseCallback('image', draw_rectangle)
 
+
+def draw_rectangle(event,x,y,flags,param):
+    global ix, iy, drawing
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print "Mouse Button Down"
+        drawing = True
+        ix, iy = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            print "Mouse Dragged"
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        print "Mouse Button Up"
+        drawing = False
+        cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+
+img = np.zeros((512,512,3), np.uint8)
+drawing = False
+ix,iy = -1,-1
 
 if __name__ == '__main__':
+    cv2.namedWindow('image')
+    point2pick()
     while(True):
-        cap = sourceCapture()
+        #img = sourceCapture()
+        cv2.imshow('image', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    cap.release()
     cv2.destroyAllWindows()
